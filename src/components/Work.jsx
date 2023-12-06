d40a4f9394fa1e1b2f3e7574294accb256ad9031
thipham2018 <trangphamtmp@gmail.com>
2023-12-06 10:04:31 -0500

    change work file

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -48,14 +48,14 @@ const Work = () => {
                     </button>
                   </a>
                   {/* eslint-disable-next-line */}
-                  <a href={item.live} target="_blank">
+                  {/* <a href={item.live} target="_blank">
                     <button
                       className="text-center rounded-lg px-4 py-3 m-2
                        bg-white text-gray-700 font-bold text-lg"
                     >
                       Live
                     </button>
-                  </a>
+                  </a> */}
                 </div>
               </div>
             </div>
