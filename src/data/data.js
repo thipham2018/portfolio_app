605a61d44afbcb80af4a54732db3b1374c320671
thipham2018 <trangphamtmp@gmail.com>
2023-12-08 09:55:39 -0500

    fix

--- src/data/data.js
+++ src/data/data.js
@@ -1,35 +1,35 @@
-// import WorkIm from '../assets/projects/workImg.jpeg'
-// import RealEst from '../assets/projects/realestate.jpg'
+import WorkIm from '../assets/projects/workImg.jpeg'
+import RealEst from '../assets/projects/realestate.jpg'
 
-// export const data = [
-//   {
-//     id: 1,
-//     name: "React JS Application",
-//     image: WorkIm,
-//     github: "",
-//     live: "",
-//   },
-//   {
-//     id: 2,
-//     name: "React JS Application",
-//     image: RealEst,
-//     github: "",
-//     live: "",
-//   },
-//   {
-//     id: 3,
-//     name: "React JS Application",
-//     image: WorkIm,
-//     github: "",
-//     live: "",
-//   },
-//   {
-//     id: 4,
-//     name: "React JS Application",
-//     image: RealEst,
-//     github: "",
-//     live: "",
-//   },
+export const data = [
+  {
+    id: 1,
+    name: "React JS Application",
+    image: WorkIm,
+    github: "",
+    live: "",
+  },
+  {
+    id: 2,
+    name: "React JS Application",
+    image: RealEst,
+    github: "",
+    live: "",
+  },
+  {
+    id: 3,
+    name: "React JS Application",
+    image: WorkIm,
+    github: "",
+    live: "",
+  },
+  {
+    id: 4,
+    name: "React JS Application",
+    image: RealEst,
+    github: "",
+    live: "",
+  },
 
 
-// ]
+]
