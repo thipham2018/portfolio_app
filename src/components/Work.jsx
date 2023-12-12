e077ec6d1ca53bcab1dfead164000d740b2d5df7
thipham2018 <trangphamtmp@gmail.com>
2023-12-12 13:56:12 -0500

    add all

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -44,7 +44,7 @@ const Work = () => {
                       className="text-center rounded-lg px-4 py-3 m-2
                        bg-white text-gray-700 font-bold text-lg"
                     >
-                      Code
+                      Project
                     </button>
                   </a>
                   {/* eslint-disable-next-line */}
