70bdf7af58b65e07551ad99a5c5f2b987b3d9ce5
thipham2018 <trangphamtmp@gmail.com>
2023-12-12 14:16:24 -0500

    change homr file

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,8 +15,7 @@ const Home = () => {
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
           I’m a full-stack developer specializing in building (and occasionally
-          designing). Currently, I’m focused on
-          building responsive full-stack web applications.
+          designing). Currently, I’m focused on building responsive full-stack web applications.
         </p>
         <div>
           <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
