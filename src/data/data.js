684b2d0b2df3b89ed1cf4819097daf333765915e
thipham2018 <trangphamtmp@gmail.com>
2023-12-19 15:09:21 -0500

    add the photos

--- src/data/data.js
+++ src/data/data.js
@@ -3,15 +3,15 @@ import RealEst from '../assets/projects/realestate.jpg'
 import Delete from '../assets/projects/delete.png'
 
 export const data = [
+  // {
+  //   id: 1,
+  //   name: "React JS Application",
+  //   image: WorkIm,
+  //   github: "https://github.com/thipham2018/BBC_Newsreader",
+  //   live: "",
+  // },
   {
     id: 1,
-    name: "React JS Application",
-    image: WorkIm,
-    github: "https://github.com/thipham2018/BBC_Newsreader",
-    live: "",
-  },
-  {
-    id: 5,
     name: "abc",
     image: Delete,
     github: "",
