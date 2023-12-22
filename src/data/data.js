aa019c749f6f878e4610c54b2436769643590cf2
thipham2018 <trangphamtmp@gmail.com>
2023-12-22 14:39:26 -0500

    add the links to github

--- src/data/data.js
+++ src/data/data.js
@@ -28,7 +28,7 @@ export const data = [
     id: 3,
     name: "React JS Application",
     image: WorkIm,
-    github: "",
+    github: "https://github.com/thipham2018/portfolio_app",
     live: "",
   },
   {
